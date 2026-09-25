59947c6b095a132d22c8e05f05d7015f1b27651f
arman <arman.arfat@gmail.com>
2026-09-25 22:39:16 +0600

    border remove

--- app/(main)/cart/page.js
+++ app/(main)/cart/page.js
@@ -135,7 +135,7 @@ export default function CartPage() {
           </div>
         ) : (
           <div
-            className="grid grid-cols-1 lg:grid-cols-3 gap-12 border border-amber-700"
+            className="grid grid-cols-1 lg:grid-cols-3 gap-12"
             style={{ fontFamily: "'Inter', sans-serif" }}
           >
             <div className="lg:col-span-2">
